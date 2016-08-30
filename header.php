c2329a075566e354f83ed49216fb2d1686f407b2
Scott Campbell <skorpion352@hotmail.com>
2016-08-30 18:31:08 +1200

    Fixed nav styling error

--- header.php
+++ header.php
@@ -44,6 +44,7 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'skorpius' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			<div id="header-spacer"></div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
